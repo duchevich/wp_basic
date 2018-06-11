cc26463b33eeaddbd723a8155bbebfb8ab2440f1
duchevich <duchevich274@gmail.com>
2018-06-11 15:08:21 +0300

    v 1.0.0

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+    //подключение стилей
+    function my_theme_styles(){
+        wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', [], '1', 'all');
+        wp_enqueue_style('bootstrap');
+        wp_register_style('bootstrap-theme', get_template_directory_uri() . '/css/bootstrap-theme.min.css', [], '1', 'all');
+        wp_enqueue_style('bootstrap-theme');
+        wp_register_style('slick', get_template_directory_uri() . '/css/slick.css', [], '1', 'all');
+        wp_enqueue_style('slick');
+        wp_register_style('slick-theme', get_template_directory_uri() . '/css/slick-theme.css', [], '1', 'all');
+        wp_enqueue_style('slick-theme');
+        wp_register_style('style', get_template_directory_uri() . '/css/style.css', [], '1', 'all');
+        wp_enqueue_style('style');
+        wp_register_style('mediaqueries', get_template_directory_uri() . '/css/mediaqueries.css',[], '1', 'all');
+        wp_enqueue_style('mediaqueries');
+    }
+    add_action('wp_enqueue_scripts', 'my_theme_styles');
+
+    // подключение скриптов
+    function my_theme_scripts(){
+        // wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD3q9ElXkQ_iW7tT24yWufEvhFM_Rclbiw&callback=initMap', '', '',true);
+        wp_enqueue_script('jquery');
+        wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', [], '1', true);
+        wp_enqueue_script('bootstrap');
+        wp_register_script('slick', get_template_directory_uri() . '/js/slick.min.js', [], '1', true);
+        wp_enqueue_script('slick');
+        wp_register_script('jquery.inputmask', get_template_directory_uri() . '/js/jquery.inputmask.js', [], '1', true);
+        wp_enqueue_script('jquery.inputmask');
+        wp_register_script('js', get_template_directory_uri() . '/js/main.js', [], '1', true);
+        wp_enqueue_script('js');
+    }
+    add_action('wp_enqueue_scripts', 'my_theme_scripts');
