b34dda259ef6a5ddd668048fe4633a4a309f15ba
Tetyana Duchevych <duchevich274@gmail.com>
2019-01-23 18:04:10 +0200

    v 1.0.2

--- front-page.php
+++ front-page.php
@@ -1,4 +1,128 @@
 <?php get_header(); ?>
+<header class="header">
+    <div class="container">
+    
+    </div>
+</header>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+    
+    </div>
+</section>
+<footer class="footer">
+    <div class="container">
+    
+    </div>
+</footer>
+<div class="modal fade" id="headerModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                    <p class="modal-title">Оставьте заявку!</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+<div class="modal fade" id="thanksModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <p class="modal-title">Спасибо за заявку</p>
+                <p class="modal-text">Мы перезвоним Вам на протяжении дня.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="docs-modal modal fade" id="politicModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <h2>Политика конфиденциальности</h2>
+                
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
